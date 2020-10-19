23b50da5b393188d0dd7f1c294c48774a9dab199
Jeff Simonson <jeffsimonson@live.com>
2020-10-19 00:17:04 -0500

    version 1.0.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# willies-reimagined-basic
+
+# Willie's Reimagined = Basic Javascript Version
+
+Willie’s Wine Bar & Cuisine was a delightful bar and restaurant located in the Loring Park neighborhood of downtown Minneapolis, MN. My wife at the time was the owner and it was her passion project. It operated from 2006-2007. Unfortunately, the location did not generate as much traffic as hoped for and was forced to close even though those who found it raved about it.
+
+There was a simple brochure-style website at the time, but it was not anything like what modern restaurants use. This project is my reimaging what the site might look like if Willie’s was around today. Whenever possible, I used actual photos that were taken at the time.
+
+From a technical perspective, the purpose of this project was to demonstrate my ability to develop a commercial website using HTML5, CSS3, and “vanilla” JavaScript. A template from Colorlib was the initial inspiration for the project. I also used the following JavaScript code libraries/plug-ins:
+
+- jQuery
+- Bootstrap
+- Animsition
+- Popper
+- Select2
+- Parallax100
+- Lightbox
---
+++ includes/contact-form.php
@@ -0,0 +1,39 @@
+<?php
+
+$string = file_get_contents("config.json");
+$option = json_decode($string);
+
+define("MAIL_HOST", $option->MAIL_HOST);
+define("MAIL_TITLE", $option->MAIL_TITLE);
+
+if( isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $msg = nl2br($_POST['message']);
+    if (MAIL_HOST != null) {
+        $to = MAIL_HOST;
+    } else {
+        $to = "rockford@excite.com";
+    }
+    $from = $email;
+    if (MAIL_TITLE != null) {
+        $subject = MAIL_TITLE;
+    } else {
+        $subject = '[Willie\'s Wine Bar & Cuisine] Contact Form Message';
+    }
+    $message = '<b>Name:</b> '.$name.' <br><b>Email:</b> '.$email.' <br> <p>'.$msg.'</p>';
+    $headers = "From: $from\n";
+    $headers .= "MIME-Version: 1.0\n";
+    $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+
+    // if( mail($to, $subject, $message, $headers) ) {
+    //     $serialized_data = '{"type":"success", "message":"Contact form successfully submitted. Thank you, I will get back to you soon!"}';
+    //     echo $serialized_data;
+    // } else {
+    //     $serialized_data = '{"type":"danger", "message":"Contact form failed. Please send again later!"}';
+    //     echo $serialized_data;
+    // }
+
+    $serialized_data = '{"type":"success", "message":"Contact form successfully submitted. Thank you, I will get back to you soon!"}';
+    echo $serialized_data;
+};
